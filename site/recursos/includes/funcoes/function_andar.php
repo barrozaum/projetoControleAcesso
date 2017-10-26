6900da36208d17ce876700bbbd08897983350f34
Igor <Igor@Barrozaum>
2017-10-26 17:10:14 -0200

    1 - ADICIONADO O ANDAR TERREO

--- site/recursos/includes/funcoes/function_andar.php
+++ site/recursos/includes/funcoes/function_andar.php
@@ -12,6 +12,10 @@ function func_descricao_andar($andar) {
         case 3 :
             return "TERCEIRO";
 
+        case 4 :
+            return "TERREO";
+
+
         default :
             return "ERROR";
     }
@@ -19,13 +23,15 @@ function func_descricao_andar($andar) {
 
 function func_ordem_andar($andar = "") {
     if ($andar == "") {
-        $andares = array("" => "SELECIONE O ANDAR", "1" => "PRIMEIRO", "2" => "SEGUNDO", "3" => "TERCEIRO");
+        $andares = array("" => "SELECIONE O ANDAR", "1" => "PRIMEIRO", "2" => "SEGUNDO", "3" => "TERCEIRO", "4" => "TERREO");
     } else if ($andar == 1) {
-        $andares = array("1" => "PRIMEIRO", "2" => "SEGUNDO", "3" => "TERCEIRO");
+        $andares = array("1" => "PRIMEIRO", "2" => "SEGUNDO", "3" => "TERCEIRO", "4" => "TERREO");
     } else if ($andar == 2) {
-        $andares = array("2" => "SEGUNDO", "1" => "PRIMEIRO", "3" => "TERCEIRO");
+        $andares = array("2" => "SEGUNDO", "1" => "PRIMEIRO", "3" => "TERCEIRO", "4" => "TERREO");
+    } else if ($andar == 3) {
+        $andares = array("3" => "TERCEIRO", "1" => "PRIMEIRO", "2" => "SEGUNDO", "4" => "TERREO");
     } else {
-        $andares = array("3" => "TERCEIRO", "1" => "PRIMEIRO", "2" => "SEGUNDO");
+        $andares = array("4" => "TERREO", "3" => "TERCEIRO", "1" => "PRIMEIRO", "2" => "SEGUNDO");
     }
 
     return $andares;
